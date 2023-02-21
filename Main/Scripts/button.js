80f22751891b02f0eec675aa8f291481096e60a7
fruitsamurai-png <64308011+fruitsamurai-png@users.noreply.github.com>
2023-02-21 11:25:35 +0800

    continue later....

---
+++ Main/Scripts/button.js
@@ -0,0 +1,8 @@
+// Get the button element by its id
+const myButton = document.getElementById('schedule');
+
+// Add a click event listener to the button
+myButton.addEventListener('click', function() {
+  // Navigate to another page
+  console.log("hello");
+});
